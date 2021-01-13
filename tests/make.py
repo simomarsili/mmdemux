5a0bcd5d9843f18598b45aa674d7d8d17b397720
simomarsili <simo.marsili@gmail.com>
2021-01-13 18:33:17 +0100

    add energy_function

--- tests/make.py
+++ tests/make.py
@@ -7,45 +7,57 @@ from openmmtools import mcmc, states, testsystems
 from openmmtools.multistate import MultiStateReporter, ParallelTemperingSampler
 from simtk import unit
 
-stem = 'test_repex'
+
+def energy_function(test, topology=None, system=None, positions=None):
+    """Potential energy for TestSystem object."""
+    topology = topology or test.topology
+    system = system or test.system
+    positions = positions or test.positions
+    platform = mm.Platform.getPlatformByName('CPU')
+    properties = {}
+    integrator = mm.VerletIntegrator(0.002 * unit.picoseconds)
+    simulation = mm.app.Simulation(top, system, integrator, platform,
+                                   properties)
+    simulation.context.setPositions(positions)
+    state = simulation.context.getState(getEnergy=True)
+    ene = state.getPotentialEnergy().value_in_unit(unit.kilojoule_per_mole)
+    return ene
+
+
+stem = 'text_repex'
 
 testsystem = testsystems.AlanineDipeptideImplicit()
-
-topology = testsystem.topology
-system = testsystem.system
-positions = testsystem.positions
+# testsystem = testsystems.SrcImplicit()
+# testsystem = testsystems.HostGuestVacuum()
 
 # save topology as .pdb
-top = mdtraj.Topology.from_openmm(topology)
-trj = mdtraj.Trajectory([positions / unit.nanometers], top)
+top = mdtraj.Topology.from_openmm(testsystem.topology)
+trj = mdtraj.Trajectory([testsystem.positions / unit.nanometers], top)
 trj.save(stem + '.pdb')
 
 # save system as .xml
-serialized_system = mm.openmm.XmlSerializer.serialize(system)
+serialized_system = mm.openmm.XmlSerializer.serialize(testsystem.system)
 with open(stem + '.xml', 'w') as fp:
     print(serialized_system, file=fp)
 
 n_replicas = 3  # Number of temperature replicas.
 T_min = 298.0 * unit.kelvin  # Minimum temperature.
 T_max = 600.0 * unit.kelvin  # Maximum temperature.
-reference_state = states.ThermodynamicState(system=system, temperature=T_min)
+reference_state = states.ThermodynamicState(system=testsystem.system,
+                                            temperature=T_min)
 
 move = mcmc.GHMCMove(timestep=2.0 * unit.femtoseconds, n_steps=50)
-simulation = ParallelTemperingSampler(mcmc_moves=move,
-                                      number_of_iterations=float('inf'),
-                                      online_analysis_interval=None)
+sampler = ParallelTemperingSampler(mcmc_moves=move,
+                                   number_of_iterations=float('inf'),
+                                   online_analysis_interval=None)
 
 storage_path = stem + '.nc'
 reporter = MultiStateReporter(storage_path, checkpoint_interval=1)
-simulation.create(reference_state,
-                  states.SamplerState(positions),
-                  reporter,
-                  min_temperature=T_min,
-                  max_temperature=T_max,
-                  n_temperatures=n_replicas)
+sampler.create(reference_state,
+               states.SamplerState(testsystem.positions),
+               reporter,
+               min_temperature=T_min,
+               max_temperature=T_max,
+               n_temperatures=n_replicas)
 
-simulation.run(n_iterations=10)
-
-trj = mdtraj.load(stem + '.pdb')
-top = trj.topology.to_openmm()
-print(top == topology)
+sampler.run(n_iterations=10)
